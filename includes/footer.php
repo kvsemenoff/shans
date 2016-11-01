95702c7000ee02b6c3dece4652ed8eae1b23a506
Berzeg07 <berezg86@gmail.com>
2016-11-01 11:31:37 +0300

    17.199

--- includes/footer.php
+++ includes/footer.php
@@ -2,16 +2,16 @@
     <div class="container">
     	<div class="row">
     		<div class="col-md-5">
-    		ff
+    			<div class="dd-footer-left">
+	    			&copy; Шанс – центр аппаратной косметологии
+    			</div>
     		</div>
     		<div class="col-md-4">
-	    		<div class="dd-vhod-bottom">
-					<div class="dd-search-box">
-						<form class="dd-form" action="#">
-							<input type="text" placeholder="Поиск">
-							<input type="submit" value="">
-						</form>
-					</div>
+				<div class="dd-search-box">
+					<form class="dd-form" action="#">
+						<input type="text" placeholder="Поиск">
+						<input type="submit" value="">
+					</form>
 				</div>
     		</div>
     		<div class="col-md-3">
