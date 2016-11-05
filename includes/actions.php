81071bb6f4bff9941a29ce850afbed411e0f77c7
Залим Лампежев <sabolch@yandex.ru>
2016-11-05 15:35:08 +0300

    actions.php

---
+++ includes/actions.php
@@ -0,0 +1,45 @@
+<section class="dg-page">
+	<div class="container dg-width">
+		<div class="row">
+			<h1>Акции</h1>
+			<div class="col-md-12">
+				tabs
+			</div>
+			<div class="dg-actions">
+				<div class="col-md-4 col-sm-6 col-xs-12 mh">
+					<h2>Избавляем от нежелательных волос навсегда!</h2>
+					<div class="calendar"><span class="left">5.10 - 5.11</span><span class="right">15%</span></div>
+					<p>Лазерная эпиляция в Тольятти в Центре косметологии «Шанс» позволит навсегда избавиться от 90% волос, избежав неприятных побочных эффектов.</p>
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12 mh">
+					<h2>Избавляем от нежелательных волос навсегда!</h2>
+					<div class="calendar"><span class="left">5.10 - 5.11</span><span class="right">15%</span></div>
+					<p>Возраст женщины можно определить по состоянию её кожи, особенно — кожи лица. Один из самых популярных способов справиться с морщинами сегодня — это ботокс.</p>
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12 mh">
+					<h2>Избавляем от нежелательных волос навсегда!</h2>
+					<div class="calendar"><span class="left">5.10 - 5.11</span><span class="right">15%</span></div>
+					<p>Давно доказано, что лучшие друзья девушек после тридцати – вовсе не бриллианты, а… антиоксиданты.</p>
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12 mh">
+					<h2>Избавляем от нежелательных волос навсегда!</h2>
+					<div class="calendar"><span class="left">5.10 - 5.11</span><span class="right">15%</span></div>
+					<p>Лазерная эпиляция в Тольятти в Центре косметологии «Шанс» позволит навсегда избавиться от 90% волос, избежав неприятных побочных эффектов.</p>
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12 mh">
+					<h2>Избавляем от нежелательных волос навсегда!</h2>
+					<div class="calendar"><span class="left">5.10 - 5.11</span><span class="right">15%</span></div>
+					<p>Возраст женщины можно определить по состоянию её кожи, особенно — кожи лица. Один из самых популярных способов справиться с морщинами сегодня — это ботокс.</p>
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12 mh">
+					<h2>Избавляем от нежелательных волос навсегда!</h2>
+					<div class="calendar"><span class="left">5.10 - 5.11</span><span class="right">15%</span></div>
+					<p>Давно доказано, что лучшие друзья девушек после тридцати – вовсе не бриллианты, а… антиоксиданты.</p>
+				</div>
+				
+			</div>
+		</div>
+		<p class="dg-center">*- подробности акции уточняйте у администраторов центра</p>
+	</div>
+</section>
+
