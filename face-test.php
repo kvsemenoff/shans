2b0b18f8e1d362d8b2073189e341cdc9d4051942
unknown <anzarsh@mail.ru>
2016-11-07 16:24:19 +0300

    sdf

--- face-test.php
+++ face-test.php
@@ -1,5 +1,5 @@
 <?php require_once('includes/header-top.php'); ?>
 <?php require_once('includes/header.php'); ?>
-<?php require_once('includes/goods.php'); ?>
+<?php require_once('includes/face-test.php'); ?>
 <?php require_once('includes/footer.php'); ?>
 
