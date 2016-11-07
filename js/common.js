31a677366da886655216a889d73beca772080075
Berzeg07 <berezg86@gmail.com>
2016-11-07 17:44:01 +0300

    17.19504

--- js/common.js
+++ js/common.js
@@ -2,42 +2,36 @@
 
 $(document).ready(function(){
 
-    $('.az-select').each(function(){
-        var select = $(this);    
-        var option = select.find('select option');
-        var str = '<div class="az-options">';
-        select.find('option').each(function(){
-            str+= '<div data-val="' +$(this).val() + '">' + $(this).text() + '</div>'
-        });
-        str+= '</div>';
-        select.html(select.html() + str);
+  var owl = $(".dd-slider");
+ 
+  owl.owlCarousel({
 
-        select.find('select').mousedown(function(){
-            return false;
-        });
-        select.mouseup(function(){
-            select.find('select').focus();
-        });
-        select.find('.az-options div[data-val]').click(function(){
-            select.find('select').val($(this).attr('data-val'));
-        });
-        select.find('select').focusout(function(){
-            if(!select.is(':hover')){
-                select.find('.az-options').slideUp(0);
-                select.removeClass('az-select-focus');
-            }
-        });
-    });
-
-    $(".az-select").click(function(){
-        $(this).find('.az-options').slideToggle(0);
-        $(this).toggleClass('az-select-focus');
-    });
-
-
-
-
+    loop:true,//Зацикливаем слайдер
+    nav:true, //Навигация включена
+    autoplay:false,//автозапуск
+    smartSpeed:1000,//Время движения
+    margin:0,
+    navText:['<span class="prev-ars1"></span>','<span class="next-ars1"></span>'],
+    responsive:{
+        0:{
+          items:1
+        },
+        380:{
+          items:1
+        },
+        600:{
+          items:1
+        },
+        1000:{
+          items:1
+        },
+         1200:{
+          items:1
+        }
+    }
      
+  });
+  
 
  });
 
