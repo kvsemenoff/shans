a635e8b266938e82a54c4dc9f8b7d12bf62cf652
Залим Лампежев <sabolch@yandex.ru>
2016-11-08 12:24:30 +0300

    style6

--- articles.php
+++ articles.php
@@ -1,4 +1,11 @@
-﻿<?php require_once('includes/header-top.php'); ?>
+﻿<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Статьи</title>
+
+<?php require_once('includes/header-top.php'); ?>
 ﻿<?php require_once('includes/header-top3.php'); ?>
 <?php require_once('includes/header.php'); ?>
 <?php require_once('includes/articles.php'); ?>
