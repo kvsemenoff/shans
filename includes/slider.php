a10ab1d921a1ad068c4831ec95391ba097fc3a93
Berzeg07 <berezg86@gmail.com>
2016-11-08 12:31:32 +0300

    17.199104

--- includes/slider.php
+++ includes/slider.php
@@ -1,9 +1,70 @@
-<section>
+<!-- <section>
 	<div class="container container_style2">
 		<div class="row">
-			<div class="col-md-12">
-				slider
+
+		</div>
+	</div>
+</section> -->
+
+<section class="dd-slider-face dd-slider-face2">
+	<div class="dd-slider">
+		<div class="item" style="background:url(../img/sl2.jpg);" >
+			<div class="dd-slide-txt-wrap2">
+				<div class="dd-logo-box">
+					<img class="img-responsive" src="img/dd-logo.png" alt="">
+				</div>
+				<div class="dd-second-txt2">
+					Кислородная
+					терапия
+				</div>
+				<div class="dd-ramk">
+					<span>Bio-Oxigen</span>
+					<!-- <a href="#">Узнайте подробности &rarr;</a> -->
+				</div>
+				<div class="dd-small-txt">
+					<a href="#">Узнайте подробности </a><span>&rarr;</span>
+				</div>
+				<div class="clearfix"></div>
 			</div>
+		</div>
+		<div class="item" style="background:url(../img/sl2.jpg);" >
+			<div class="dd-slide-txt-wrap2">
+				<div class="dd-logo-box">
+					<img class="img-responsive" src="img/dd-logo.png" alt="">
+				</div>
+				<div class="dd-second-txt2">
+					Кислородная
+					терапия
+				</div>
+				<div class="dd-ramk">
+					<span>Bio-Oxigen</span>
+					<!-- <a href="#">Узнайте подробности &rarr;</a> -->
+				</div>
+				<div class="dd-small-txt">
+					<a href="#">Узнайте подробности </a><span>&rarr;</span>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		<div class="item" style="background:url(../img/sl2.jpg);" >
+			<div class="dd-slide-txt-wrap2">
+				<div class="dd-logo-box">
+					<img class="img-responsive" src="img/dd-logo.png" alt="">
+				</div>
+				<div class="dd-second-txt2">
+					Кислородная
+					терапия
+				</div>
+				<div class="dd-ramk">
+					<span>Bio-Oxigen</span>
+					<!-- <a href="#">Узнайте подробности &rarr;</a> -->
+				</div>
+				<div class="dd-small-txt">
+					<a href="#">Узнайте подробности </a><span>&rarr;</span>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
 		</div>
 	</div>
 </section>
