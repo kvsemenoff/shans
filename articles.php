c51c81983bc170a49f5233ff35b1beb12ed487bb
Залим Лампежев <sabolch@yandex.ru>
2016-11-08 11:52:08 +0300

    style6

--- articles.php
+++ articles.php
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 ﻿<?php require_once('includes/header-top.php'); ?>
-=======
 ﻿<?php require_once('includes/header-top3.php'); ?>
->>>>>>> fef2d16b0f66bae305cd942068e877e93adb1bcc
 <?php require_once('includes/header.php'); ?>
 <?php require_once('includes/articles.php'); ?>
 <?php require_once('includes/footer.php'); ?>
